2add27f0c89b79fd8807bed005e039027ace5395
YOUR NAME <YOUR@EMAIL.com>
2018-11-10 16:54:27 -0500

    first commit

---
+++ RocketApp/ElevatorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ElevatorViewController.swift
+//  RocketApp
+//
+//  Created by Frederique Grenier on 2018-11-07.
+//  Copyright © 2018 Frederique Grenier. All rights reserved.
+//
+
+import UIKit
+
+class ElevatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
